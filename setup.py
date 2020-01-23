151fd9a0df2bed9c1d4196d531cea2691e572b30
Abhilash Sarwade <as.sarwade@gmail.com>
2020-01-23 11:59:15 +0530

    Added numpy and os as essential requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-#requirements = ['Click>=7.0', ]
+requirements = ['numpy','os']
 
 setup_requirements = [ ]
 
@@ -37,7 +37,7 @@ setup(
             'solexs_pipeline=solexs_pipeline.cli:main',
         ],
     },
-    #install_requires=requirements,
+    install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
     include_package_data=True,
